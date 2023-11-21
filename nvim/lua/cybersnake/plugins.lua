3077a3d7c6ef17be0665fbd17132a4bfc634b844
Cybersnake223 <143771512+Cybersnake223@users.noreply.github.com>
2023-11-22 00:29:58 +0530

    Updates

---
+++ nvim/lua/cybersnake/plugins.lua
@@ -0,0 +1,125 @@
+-- ############################################################################################################ --
+-- ##   ______  __      __  _______   ________  _______    ______   __    __   ______   __    __  ________   ## --
+-- ##  /      \|  \    /  \|       \ |        \|       \  /      \ |  \  |  \ /      \ |  \  /  \|        \  ## --
+-- ## |  $$$$$$\\$$\  /  $$| $$$$$$$\| $$$$$$$$| $$$$$$$\|  $$$$$$\| $$\ | $$|  $$$$$$\| $$ /  $$| $$$$$$$$  ## --
+-- ## | $$   \$$ \$$\/  $$ | $$__/ $$| $$__    | $$__| $$| $$___\$$| $$$\| $$| $$__| $$| $$/  $$ | $$__      ## --
+-- ## | $$        \$$  $$  | $$    $$| $$  \   | $$    $$ \$$    \ | $$$$\ $$| $$    $$| $$  $$  | $$  \     ## --
+-- ## | $$   __    \$$$$   | $$$$$$$\| $$$$$   | $$$$$$$\ _\$$$$$$\| $$\$$ $$| $$$$$$$$| $$$$$\  | $$$$$     ## --
+-- ## | $$__/  \   | $$    | $$__/ $$| $$_____ | $$  | $$|  \__| $$| $$ \$$$$| $$  | $$| $$ \$$\ | $$_____   ## --
+-- ##  \$$    $$   | $$    | $$    $$| $$     \| $$  | $$ \$$    $$| $$  \$$$| $$  | $$| $$  \$$\| $$     \  ## --
+-- ##   \$$$$$$     \$$     \$$$$$$$  \$$$$$$$$ \$$   \$$  \$$$$$$  \$$   \$$ \$$   \$$ \$$   \$$ \$$$$$$$$  ## --
+-- ##                                                                                                        ## --
+-- ## Created by Cybersnake                                                                                  ## --
+-- ############################################################################################################ --
+
+--- Bootstraping Lazy Package Manager ---
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--- ### Plugins Setup ### ---
+  
+  --- Neoconf ---
+
+require("lazy").setup({
+  { "folke/neoconf.nvim", cmd = "Neoconf" , lazy = true },
+  "folke/neodev.nvim",
+
+  --- ColorScheme ---
+
+  { 'Shatur/neovim-ayu',
+    as = 'ayu-dark',
+    config = function()
+        vim.cmd('colorscheme ayu-dark')
+end };
+
+  --- Mason Lsp ---
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    cmd = { "LspInstall", "LspUninstall" },
+    config = function()
+      require("mason-lspconfig").setup(lvim.lsp.installer.setup)
+
+      -- automatic_installation is handled by lsp-manager
+      local settings = require "mason-lspconfig.settings"
+      settings.current.automatic_installation = true
+    end,
+    lazy = true,
+    event = "User FileOpened",
+    dependencies = "mason.nvim",
+  },
+
+  --- Telescope ---
+  
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    config = function()
+    end,
+    dependencies = { "telescope-fzf-native.nvim" },
+    lazy = true,
+    cmd = "Telescope",
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    lazy = true,
+  },
+
+  --- Treesitter ---  
+  
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- run = ":TSUpdate",
+    config = function()
+      local utils = require "lvim.utils"
+      local path = utils.join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt", "nvim-treesitter")
+      vim.opt.rtp:prepend(path) -- treesitter needs to be before nvim's runtime in rtp
+      require("lvim.core.treesitter").setup()
+    end,
+    cmd = {
+      "TSInstall",
+      "TSUninstall",
+      "TSUpdate",
+      "TSUpdateSync",
+      "TSInstallInfo",
+      "TSInstallSync",
+      "TSInstallFromGrammar",
+    },
+    event = "User FileOpened",
+  },
+
+-- Project ---
+{
+  "ahmedkhalf/project.nvim",
+    lazy = true,
+  config = function()
+    require("project_nvim").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+},
+  -- Icons
+{
+    'nvim-tree/nvim-web-devicons',
+    lazy = true,
+  },
+
+--- Transparency ---
+{ "xiyaowong/transparent.nvim",
+  lazy = false,
+}
+})
