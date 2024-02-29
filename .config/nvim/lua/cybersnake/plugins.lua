0db347578191a79b6afe102140c12483ee5fe458
Cybersnake223 <Cybersnake223@gmail.com>
2024-03-01 03:16:31 +0530

    Updates

--- .config/nvim/lua/cybersnake/plugins.lua
+++ .config/nvim/lua/cybersnake/plugins.lua
@@ -156,4 +156,21 @@ end };
     'akinsho/bufferline.nvim', version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons'
 },
+
+  --- Noice ---
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+},
 })
