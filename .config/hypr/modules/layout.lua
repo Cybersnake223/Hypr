8524b228a963b339f6aefee2f019d10188cfea0c
Cybersnake <143771512+Cybersnake223@users.noreply.github.com>
2026-05-14 07:15:09 +0530

    add: scrolling layout conf

--- .config/hypr/modules/layout.lua
+++ .config/hypr/modules/layout.lua
@@ -13,4 +13,15 @@ hl.config({
     master = {
         new_status = "dwindle",
     },
+
+    scrolling = {
+        column_width             = 0.9,
+        fullscreen_on_one_column = true,
+        focus_fit_method         = 1,
+        follow_focus             = true,
+        follow_min_visible       = 0.9,
+        wrap_focus               = true,
+        wrap_swapcol             = true,
+        direction                = "right",
+    }
 })
